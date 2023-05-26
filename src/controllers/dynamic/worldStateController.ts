52a1b55309d1963b65b0e18770c20c47f35528db
Master <master@orzsoft.com>
2023-05-26 11:08:49 +0800

    update

--- src/controllers/dynamic/worldStateController.ts
+++ src/controllers/dynamic/worldStateController.ts
@@ -1,7 +1,10 @@
 import { RequestHandler } from "express";
+import config from "@/config.json";
 import worldState from "@/static/fixed_responses/worldState.json";
 
 const worldStateController: RequestHandler = (_req, res) => {
+    worldState.WorldSeed = config.WorldSeed;
+    worldState.BuildLabel = config.buildLabel;
     res.json(worldState);
 };
 
