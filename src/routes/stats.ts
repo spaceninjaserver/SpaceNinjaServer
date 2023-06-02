045542a321cc84e18ace6202d7093557c9ee686d
AKCore <AKCore@users.noreply.github.com>
2023-06-01 18:01:02 -0700

    Update src/routes/stats.ts
    Co-authored-by: Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -3,6 +3,5 @@ import { uploadController } from "../controllers/stats/uploadController";
 
 const statsRouter = express.Router();
 
-//post 
 statsRouter.post("/upload.php", uploadController); 
 export { statsRouter };
