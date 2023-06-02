f4412a7aad18f0bf627260296be4d51e9439a025
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2023-06-01 22:39:42 -0300

    remove mistake

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,35 +1,32 @@
-import {
-    checkDailyMissionBonusController,
-    deleteSessionController,
-    dronesController,
-    findSessionsController,
-    genericUpdateController,
-    getAllianceController,
-    getCreditsController,
-    getFriendsController,
-    getIgnoredUsersController,
-    getNewRewardSeedController,
-    getShipController,
-    hostSessionController,
-    hubController,
-    hubInstancesController,
-    inboxController,
-    inventoryController,
-    loginController,
-    loginRewardsController,
-    logoutController,
-    marketRecommendationsController,
-    missionInventoryUpdateController,
-    modularWeaponSaleController,
-    purchaseController,
-    rerollRandomModController,
-    setActiveQuestController,
-    surveysController,
-    updateChallengeProgressController,
-    updateSessionGetController,
-    updateSessionPostController,
-    viewController
-} from "@/src/controllers/api";
+import { checkDailyMissionBonusController } from "@/src/controllers/api/checkDailyMissionBonusController";
+import { deleteSessionController } from "@/src/controllers/api/deleteSessionController";
+import { dronesController } from "@/src/controllers/api/dronesController";
+import { findSessionsController } from "@/src/controllers/api/findSessionsController";
+import { genericUpdateController } from "@/src/controllers/api/genericUpdateController";
+import { getAllianceController } from "@/src/controllers/api/getAllianceController";
+import { getCreditsController } from "@/src/controllers/api/getCreditsController";
+import { getFriendsController } from "@/src/controllers/api/getFriendsController";
+import { getIgnoredUsersController } from "@/src/controllers/api/getIgnoredUsersController";
+import { getNewRewardSeedController } from "@/src/controllers/api/getNewRewardSeedController";
+import { getShipController } from "@/src/controllers/api/getShipController";
+import { hostSessionController } from "@/src/controllers/api/hostSessionController";
+import { hubController } from "@/src/controllers/api/hubController";
+import { hubInstancesController } from "@/src/controllers/api/hubInstancesController";
+import { inboxController } from "@/src/controllers/api/inboxController";
+import { inventoryController } from "@/src/controllers/api/inventoryController";
+import { loginController } from "@/src/controllers/api/loginController";
+import { loginRewardsController } from "@/src/controllers/api/loginRewardsController";
+import { logoutController } from "@/src/controllers/api/logoutController";
+import { marketRecommendationsController } from "@/src/controllers/api/marketRecommendationsController";
+import { missionInventoryUpdateController } from "@/src/controllers/api/missionInventoryUpdateController";
+import { modularWeaponSaleController } from "@/src/controllers/api/modularWeaponSaleController";
+import { purchaseController } from "@/src/controllers/api/purchaseController";
+import { rerollRandomModController } from "@/src/controllers/api/rerollRandomModController";
+import { setActiveQuestController } from "@/src/controllers/api/setActiveQuestController";
+import { surveysController } from "@/src/controllers/api/surveysController";
+import { updateChallengeProgressController } from "@/src/controllers/api/updateChallengeProgressController";
+import { updateSessionGetController, updateSessionPostController } from "@/src/controllers/api/updateSessionController";
+import { viewController } from "@/src/controllers/api/viewController";
 import express from "express";
 
 const apiRouter = express.Router();
