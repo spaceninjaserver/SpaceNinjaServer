b2e2ef5f0b61d17339c76bf9d496559f166dd38c
Master <master@orzsoft.com>
2023-06-05 04:34:58 +0800

    fix ship problem

--- src/controllers/api/getShipController.ts
+++ src/controllers/api/getShipController.ts
@@ -4,10 +4,11 @@ import { createShip } from "@/src/services/shipService";
 import { RequestHandler } from "express";
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-const getShipController: RequestHandler = async (_req, res) => {
-    const accountId = _req.query.accountId;
+const getShipController: RequestHandler = async (req, res) => {
+    const accountId = req.query.accountId;
     const ship = await Ship.findOne({ ShipOwnerId: accountId });
     if (!ship) {
+        // previously registered account
         const account = await Account.findOne({ _id: accountId });
         if (account) {
             await createShip(account._id);
