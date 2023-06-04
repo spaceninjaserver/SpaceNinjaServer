a91f9692821aa45c6cd693c029974d382a777640
OrdisPrime <134585663+OrdisPrime@users.noreply.github.com>
2023-06-05 00:17:01 +0200

    fix some ship things. (#16)

--- src/controllers/api/getShipController.ts
+++ src/controllers/api/getShipController.ts
@@ -1,20 +1,13 @@
-import { Account } from "@/src/models/loginModel";
 import { Ship } from "@/src/models/shipModel";
-import { createShip } from "@/src/services/shipService";
 import { RequestHandler } from "express";
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-const getShipController: RequestHandler = async (_req, res) => {
-    const accountId = _req.query.accountId;
+const getShipController: RequestHandler = async (req, res) => {
+    const accountId = req.query.accountId;
     const ship = await Ship.findOne({ ShipOwnerId: accountId });
     if (!ship) {
-        const account = await Account.findOne({ _id: accountId });
-        if (account) {
-            await createShip(account._id);
-            const new_ship = await Ship.findOne({ ShipOwnerId: accountId });
-            res.json(new_ship);
-            return;
-        }
+        res.status(500).json({ error: "error finding a corresponding ship" });
+        return;
     }
     res.json(ship);
 };
