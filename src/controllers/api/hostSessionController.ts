a4cd721a6a5c02fc4c69ddbd3439093a97347556
Ordis <134585663+OrdisPrime@users.noreply.github.com>
2023-06-04 02:32:35 +0200

    hopefully we good now

--- src/controllers/api/hostSessionController.ts
+++ src/controllers/api/hostSessionController.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 
 const hostSessionController: RequestHandler = (_req, res) => {
+    console.log("HostSession Request:", JSON.parse(_req.body));
+
     res.json({ sessionId: { $oid: "64768f104722f795300c9fc0" }, rewardSeed: 5867309943877621023 });
 };
 
