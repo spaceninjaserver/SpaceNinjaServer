96952d5ea6fc8f91ea1fa40156aa84833122444d
Master <master@orzsoft.com>
2023-06-05 05:55:36 +0800

    fix bug

--- src/controllers/api/getShipController.ts
+++ src/controllers/api/getShipController.ts
@@ -1,14 +1,13 @@
+import { RequestHandler } from "express";
+
 import { Account } from "@/src/models/loginModel";
 import { Ship } from "@/src/models/shipModel";
 
 import { createShip } from "@/src/services/shipService";
 
-import config from "@/config.json";
-import testShipFeature from "@/static/fixed_responses/testShipFeature.json";
-
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-const getShipController: RequestHandler = async (_req, res) => {
-    const accountId = _req.query.accountId;
+const getShipController: RequestHandler = async (req, res) => {
+    const accountId = req.query.accountId;
     const ship = await Ship.findOne({ ShipOwnerId: accountId });
     if (!ship) {
         const account = await Account.findOne({ _id: accountId });
