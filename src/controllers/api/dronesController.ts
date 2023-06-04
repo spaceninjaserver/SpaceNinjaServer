d3b12ef836c0634bf71ed8710f9500b2458f9c19
Ordis <134585663+OrdisPrime@users.noreply.github.com>
2023-06-04 02:20:25 +0200

    stash

--- src/controllers/api/dronesController.ts
+++ src/controllers/api/dronesController.ts
@@ -1,11 +1,7 @@
 import { RequestHandler } from "express";
 
 const dronesController: RequestHandler = (_req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html",
-        "Content-Length": "3"
-    });
-    res.end(Buffer.from([0x7b, 0x7d, 0x0a]));
+    res.json({});
 };
 
 export { dronesController };
