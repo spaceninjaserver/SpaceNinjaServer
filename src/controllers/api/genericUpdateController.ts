1d0c3ee1731806342051d46769d1c248597384e8
Sainan <Sainan@users.noreply.github.com>
2024-05-03 16:24:49 +0000

    Apply prettier changes

--- src/controllers/api/genericUpdateController.ts
+++ src/controllers/api/genericUpdateController.ts
@@ -6,7 +6,7 @@ import { getJSONfromString } from "@/src/helpers/stringHelpers";
 const genericUpdateController: RequestHandler = async (request, response) => {
     const accountId = request.query.accountId as string;
     const update = getJSONfromString(request.body.toString());
-   response.json(await updateGeneric(update, accountId));
+    response.json(await updateGeneric(update, accountId));
 };
 
 export { genericUpdateController };
