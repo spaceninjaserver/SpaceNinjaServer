c3156a0abf2345ac9847c75ac2f00d882d11c7a4
Sainan <sainan@calamity.gg>
2024-05-04 15:49:20 +0200

    cleanup: remove unused guild stuff

--- src/types/guildTypes.ts
+++ src/types/guildTypes.ts
@@ -5,14 +5,6 @@ export interface IGuild {
     Name: string;
 }
 
-export interface IDatabaseGuild extends IGuild {
-    _id: Types.ObjectId;
-}
-
-export interface IGuildResponse extends IGuild {
-    _id: IOid;
-}
-
 export interface ICreateGuildRequest {
     guildName: string;
 }
