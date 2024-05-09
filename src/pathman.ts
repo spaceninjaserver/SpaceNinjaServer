b79d0034e74f912b7abc71396c81830006da19f5
Sainan <sainan@calamity.gg>
2024-05-09 15:10:13 +0200

    fix: errors with `npm run build && npm run start` (#168)

---
+++ src/pathman.ts
@@ -0,0 +1,12 @@
+// Hooks node to support require from "@/" paths for `npm run build && npm run start`.
+// Based on https://github.com/dividab/tsconfig-paths
+
+const Module = require("module");
+const originalResolveFilename = Module._resolveFilename;
+Module._resolveFilename = function (request: string, _parent: any): string {
+    if (request.substring(0, 2) == "@/") {
+        const modifiedArguments = [process.cwd() + "/build/" + request.substr(2), ...[].slice.call(arguments, 1)]; // Passes all arguments. Even those that is not specified above.
+        return originalResolveFilename.apply(this, modifiedArguments);
+    }
+    return originalResolveFilename.apply(this, arguments);
+};
