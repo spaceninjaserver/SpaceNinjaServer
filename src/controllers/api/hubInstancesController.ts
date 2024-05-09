a65cc1f7244f8c78ca75fa86a2b6e63aa7e6fbe0
Sainan <sainan@calamity.gg>
2024-05-09 23:14:54 +0200

    improve: skip HUB instance selection Makes the game think there's only 1 HUB instance so it won't ask us to pick one.

--- src/controllers/api/hubInstancesController.ts
+++ src/controllers/api/hubInstancesController.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 
 const hubInstancesController: RequestHandler = (_req, res) => {
-    res.json("list 50 16 1 0 scenarios 0 0 0 0 0 0");
+    res.json("list 50 1 0 0 scenarios 0 0 0 0 0 0");
 };
 
 export { hubInstancesController };
