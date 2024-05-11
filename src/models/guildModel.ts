e066f0f23f87ed8f2621aee32deec4abd1a75145
Sainan <sainan@calamity.gg>
2024-05-11 16:42:46 +0200

    fix: superfluous fields after converting guild to client representation

--- src/models/guildModel.ts
+++ src/models/guildModel.ts
@@ -2,14 +2,18 @@ import { IGuild } from "@/src/types/guildTypes";
 import { model, Schema } from "mongoose";
 import { toOid } from "@/src/helpers/inventoryHelpers";
 
-const guildSchema = new Schema<IGuild>({
-    Name: { type: String, required: true }
-});
+const guildSchema = new Schema<IGuild>(
+    {
+        Name: { type: String, required: true }
+    },
+    { id: false }
+);
 
 guildSchema.set("toJSON", {
     virtuals: true,
     transform(_document, guild) {
         guild._id = toOid(guild._id);
+        delete guild.__v;
     }
 });
 
