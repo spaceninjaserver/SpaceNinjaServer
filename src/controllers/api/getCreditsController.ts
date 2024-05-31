17244ea390feb381465fbb31eb8f655b877fd958
Sainan <sainan@calamity.gg>
2024-05-31 13:17:10 +0200

    improve: say the magic words to log the client out when nonce is invalidated (#250)

--- src/controllers/api/getCreditsController.ts
+++ src/controllers/api/getCreditsController.ts
@@ -5,6 +5,14 @@ import { getInventory } from "@/src/services/inventoryService";
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 export const getCreditsController: RequestHandler = async (req, res) => {
+    let accountId;
+    try {
+        accountId = await getAccountIdForRequest(req);
+    } catch (e) {
+        res.status(400).send("Log-in expired");
+        return;
+    }
+
     if (config.infiniteResources) {
         res.json({
             RegularCredits: 999999999,
@@ -14,8 +22,6 @@ export const getCreditsController: RequestHandler = async (req, res) => {
         });
         return;
     }
-
-    const accountId = await getAccountIdForRequest(req);
 
     const inventory = await getInventory(accountId);
     res.json({
