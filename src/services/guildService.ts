f02e4a90030f52efe7422e1d454287e11d5d6d54
Sainan <Sainan@users.noreply.github.com>
2024-06-06 13:03:42 +0000

    Apply prettier changes

--- src/services/guildService.ts
+++ src/services/guildService.ts
@@ -5,14 +5,14 @@ import { Guild } from "@/src/models/guildModel";
 
 export const getGuildForRequest = async (req: Request) => {
     const accountId = await getAccountIdForRequest(req);
-	const inventory = await getInventory(accountId);
-	const guildId = req.query.guildId as string;
-	if (!inventory.GuildId || inventory.GuildId.toString() != guildId) {
+    const inventory = await getInventory(accountId);
+    const guildId = req.query.guildId as string;
+    if (!inventory.GuildId || inventory.GuildId.toString() != guildId) {
         throw new Error("Account is not in the guild that it has sent a request for");
     }
     const guild = await Guild.findOne({ _id: guildId });
     if (!guild) {
-    	throw new Error("Account thinks it is a in guild that doesn't exist");
+        throw new Error("Account thinks it is a in guild that doesn't exist");
     }
     return guild;
 };
