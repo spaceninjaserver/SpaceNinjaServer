41f27362d254817b59a3ecfd4ddc445b4caf1db5
Sainan <sainan@calamity.gg>
2024-06-06 15:03:05 +0200

    Add getGuildForRequest

--- src/controllers/api/startDojoRecipeController.ts
+++ src/controllers/api/startDojoRecipeController.ts
@@ -1,8 +1,6 @@
 import { RequestHandler } from "express";
 import { IDojoComponentClient } from "@/src/types/guildTypes";
-import { getAccountIdForRequest } from "@/src/services/loginService";
-import { Inventory } from "@/src/models/inventoryModels/inventoryModel";
-import { Guild } from "@/src/models/guildModel";
+import { getGuildForRequest } from "@/src/services/guildService";
 import { Types } from "mongoose";
 
 interface IStartDojoRecipeRequest {
@@ -11,19 +9,8 @@ interface IStartDojoRecipeRequest {
 }
 
 export const startDojoRecipeController: RequestHandler = async (req, res) => {
-    const accountId = await getAccountIdForRequest(req);
-    const inventory = await Inventory.findOne({ accountOwnerId: accountId });
-    if (!inventory) {
-        res.status(400).end();
-        return;
-    }
-    const guildId = req.query.guildId as string;
-    if (!inventory.GuildId || inventory.GuildId.toString() != guildId) {
-        res.status(400).end();
-        return;
-    }
-    // Verified that a member of the guild is making this request. Assuming they are allowed to start a build.
-    const guild = (await Guild.findOne({ _id: guildId }))!;
+    const guild = await getGuildForRequest(req);
+    // At this point, we know that a member of the guild is making this request. Assuming they are allowed to start a build.
     const request = JSON.parse(req.body.toString()) as IStartDojoRecipeRequest;
     guild.DojoComponents!.push({
         _id: new Types.ObjectId(),
