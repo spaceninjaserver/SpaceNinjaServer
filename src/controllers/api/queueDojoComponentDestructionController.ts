c1f5a2b4649b8f3f0847244c8bad4bc0ac0b5de3
OrdisPrime <OrdisPrime@users.noreply.github.com>
2024-06-07 14:02:00 +0000

    Apply prettier changes

--- src/controllers/api/queueDojoComponentDestructionController.ts
+++ src/controllers/api/queueDojoComponentDestructionController.ts
@@ -5,7 +5,10 @@ import { RequestHandler } from "express";
 export const queueDojoComponentDestructionController: RequestHandler = async (req, res) => {
     const guild = await getGuildForRequest(req);
     const componentId = req.query.componentId as string;
-    guild.DojoComponents!.splice(guild.DojoComponents!.findIndex((x) => x._id.toString() === componentId), 1);
+    guild.DojoComponents!.splice(
+        guild.DojoComponents!.findIndex(x => x._id.toString() === componentId),
+        1
+    );
     await guild.save();
     res.json({
         DojoRequestStatus: 1
