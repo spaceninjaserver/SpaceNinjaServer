ae717cc529747d20db9ee6777d1cf86880864ff1
Master <master@orzsoft.com>
2024-06-07 21:08:43 +0800

    implement queueDojoComponentDestructionController to remove room in Dojo

---
+++ src/controllers/api/queueDojoComponentDestructionController.ts
@@ -0,0 +1,13 @@
+import { getGuildForRequest } from "@/src/services/guildService";
+import { RequestHandler } from "express";
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+export const queueDojoComponentDestructionController: RequestHandler = async (req, res) => {
+    const guild = await getGuildForRequest(req);
+    const componentId = req.query.componentId as string;
+    guild.DojoComponents!.splice(guild.DojoComponents!.findIndex((x) => x._id.toString() === componentId), 1);
+    await guild.save();
+    res.json({
+        DojoRequestStatus: 1
+    });
+};
