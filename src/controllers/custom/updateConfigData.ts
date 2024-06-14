d35172aac62d811d44dae63ced74dbd6e00d5e81
Vampire Kitten <dynamightkobold@gmail.com>
2024-06-14 02:00:41 +0200

    Fixed the same thing in updateConfigData
    Because I forgot I had also done a require for FS

--- src/controllers/custom/updateConfigData.ts
+++ src/controllers/custom/updateConfigData.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from "express";
 import path from "path";
+import fs from "fs";
 const rootDir = path.join(__dirname, "../../..");
 
 const updateConfigData: RequestHandler = async (req) => {
-    const fs = require('fs');
     const updateSettingsData = req.body;
     
     fs.writeFile(path.join(rootDir, "config.json"), updateSettingsData, function(err:any) {
