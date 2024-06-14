31469424f1edc39a4cd5793f751750b682fe0378
Vampire Kitten <dynamightkobold@gmail.com>
2024-06-14 02:03:08 +0200

    Removed Async to fix last error
    Hopefully this should work now.

--- src/controllers/custom/updateConfigData.ts
+++ src/controllers/custom/updateConfigData.ts
@@ -3,7 +3,7 @@ import path from "path";
 import fs from "fs";
 const rootDir = path.join(__dirname, "../../..");
 
-const updateConfigData: RequestHandler = async (req) => {
+const updateConfigData: RequestHandler = (req) => {
     const updateSettingsData = req.body;
     
     fs.writeFile(path.join(rootDir, "config.json"), updateSettingsData, function(err:any) {
