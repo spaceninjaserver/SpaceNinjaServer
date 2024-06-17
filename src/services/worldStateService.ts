c735b997520dbb4b47f0feb71b96dbf11f0229f3
dutlist <166175391+AMelonInsideLemon@users.noreply.github.com>
2024-06-17 03:38:10 +0200

    fix eslint?

--- src/services/worldStateService.ts
+++ src/services/worldStateService.ts
@@ -1,5 +1,4 @@
 import { WorldState } from "@/src/models/worldStateModel";
-import buildConfig from "@/static/data/buildConfig.json";
 
 export const createWorldState = async () => {
     const worldState = new WorldState();
