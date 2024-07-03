0b03bc01d439e8521e7567a336b78e392ed317da
Sainan <sainan@calamity.gg>
2024-07-03 12:34:25 +0200

    fix: Archimedean Yonta having no wares (#450)

--- src/controllers/api/getVendorInfoController.ts
+++ src/controllers/api/getVendorInfoController.ts
@@ -1,7 +1,23 @@
 import { RequestHandler } from "express";
-import getVendorInfoResponse from "@/static/fixed_responses/getVendorInfo.json";
+import ArchimedeanVendorManifest from "@/static/fixed_responses/getVendorInfo/ArchimedeanVendorManifest.json";
+import MaskSalesmanManifest from "@/static/fixed_responses/getVendorInfo/MaskSalesmanManifest.json";
+import ZarimanCommisionsManifestArchimedean from "@/static/fixed_responses/getVendorInfo/ZarimanCommisionsManifestArchimedean.json";
 
 export const getVendorInfoController: RequestHandler = (req, res) => {
-    console.assert(req.query.vendor == "/Lotus/Types/Game/VendorManifests/Ostron/MaskSalesmanManifest");
-    res.json(getVendorInfoResponse);
+    switch (req.query.vendor as string) {
+        case "/Lotus/Types/Game/VendorManifests/Zariman/ArchimedeanVendorManifest":
+            res.json(ArchimedeanVendorManifest);
+            break;
+
+        case "/Lotus/Types/Game/VendorManifests/Ostron/MaskSalesmanManifest":
+            res.json(MaskSalesmanManifest);
+            break;
+
+        case "/Lotus/Types/Game/VendorManifests/Zariman/ZarimanCommisionsManifestArchimedean":
+            res.json(ZarimanCommisionsManifestArchimedean);
+            break;
+
+        default:
+            throw new Error(`Unknown vendor: ${req.query.vendor}`);
+    }
 };
