81c1574999590f526e44e37eedde7581d608130f
Sainan <sainan@calamity.inc>
2024-10-04 15:47:12 +0200

    Remove unused cache routes

--- src/routes/cache.ts
+++ src/routes/cache.ts
@@ -4,14 +4,6 @@ import fs from "fs/promises";
 
 const cacheRouter = express.Router();
 
-cacheRouter.get("/B.Cache.Dx11.bin.*", (_req, res) => {
-    res.sendFile("static/data/B.Cache.Dx11_33.0.6.bin", { root: "./" });
-});
-
-cacheRouter.get("/B.Cache.Windows_en.bin*", (_req, res) => {
-    res.sendFile("static/data/B.Cache.Windows_en_33.0.10.bin", { root: "./" });
-});
-
 cacheRouter.get(/^\/origin\/[a-zA-Z0-9]+\/[0-9]+\/H\.Cache\.bin.*$/, (_req, res) => {
     res.sendFile(`static/data/H.Cache_${buildConfig.version}.bin`, { root: "./" });
 });
