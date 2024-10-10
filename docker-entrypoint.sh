533c249e683bc37888b7632a404e37bddfe76e53
sw5ciprl <51961935+sw5ciprl@users.noreply.github.com>
2024-10-10 23:07:37 +0300

    feat: create Docker image, set up Docker CI (#528)

---
+++ docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Set up the configuration file using environment variables.
+echo '{
+	"logger": {
+	  "files": true,
+	  "level": "trace",
+	  "__valid_levels": "fatal, error, warn, info, http, debug, trace"
+	}
+}
+' > config.json
+
+for config in $(env | grep "APP_")
+do
+  var=$(echo "${config}" | tr '[:upper:]' '[:lower:]' | sed 's/app_//g' | sed -E 's/_([a-z])/\U\1/g' | sed 's/=.*//g')
+  val=$(echo "${config}" | sed 's/.*=//g')
+  jq --arg variable "$var" --arg value "$val" '.[$variable] += $value' config.json > config.tmp
+  mv config.tmp config.json
+done
+
+if [ ! -f "/app/static/data/buildConfig.json" ]
+then
+  echo "buildConfig not found, refusing to start."
+  exit 1
+fi
+
+npm install
+exec npm run dev
