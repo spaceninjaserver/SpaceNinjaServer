48e0d6069e48195a8d35c7500cb85071ac5d72ec
Sainan <sainan@calamity.inc>
2024-12-19 04:33:01 +0100

    feat: respect client-supplied buildLabel for login & worldState requests

--- src/controllers/dynamic/worldStateController.ts
+++ src/controllers/dynamic/worldStateController.ts
@@ -2,10 +2,13 @@ import { RequestHandler } from "express";
 import worldState from "@/static/fixed_responses/worldState.json";
 import buildConfig from "@/static/data/buildConfig.json";
 
-const worldStateController: RequestHandler = (_req, res) => {
+const worldStateController: RequestHandler = (req, res) => {
+    const buildLabel: string =
+        typeof req.query.buildLabel == "string" ? req.query.buildLabel.split(" ").join("+") : buildConfig.buildLabel;
+
     res.json({
         ...worldState,
-        BuildLabel: buildConfig.buildLabel,
+        BuildLabel: buildLabel,
         Time: Math.round(Date.now() / 1000)
     });
 };
