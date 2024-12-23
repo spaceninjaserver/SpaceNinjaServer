7fdb59f6c9eecddbb276b79c6d53184306b21fa6
Sainan <sainan@calamity.inc>
2024-12-23 23:12:21 +0100

    feat: dojo room energy & capacity costs & gains (#633)

--- src/controllers/api/queueDojoComponentDestructionController.ts
+++ src/controllers/api/queueDojoComponentDestructionController.ts
@@ -1,13 +1,21 @@
 import { getGuildForRequest } from "@/src/services/guildService";
 import { RequestHandler } from "express";
+import { ExportDojoRecipes } from "warframe-public-export-plus";
 
 export const queueDojoComponentDestructionController: RequestHandler = async (req, res) => {
     const guild = await getGuildForRequest(req);
     const componentId = req.query.componentId as string;
-    guild.DojoComponents!.splice(
+    const component = guild.DojoComponents!.splice(
         guild.DojoComponents!.findIndex(x => x._id.toString() === componentId),
         1
-    );
+    )[0];
+    if (component) {
+        const room = Object.values(ExportDojoRecipes.rooms).find(x => x.resultType == component.pf);
+        if (room) {
+            guild.DojoCapacity -= room.capacity;
+            guild.DojoEnergy -= room.energy;
+        }
+    }
     await guild.save();
     res.json({
         DojoRequestStatus: 1
--- src/controllers/api/startDojoRecipeController.ts
+++ src/controllers/api/startDojoRecipeController.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import { IDojoComponentClient } from "@/src/types/guildTypes";
 import { getGuildForRequest } from "@/src/services/guildService";
 import { Types } from "mongoose";
+import { ExportDojoRecipes } from "warframe-public-export-plus";
 
 interface IStartDojoRecipeRequest {
     PlacedComponent: IDojoComponentClient;
@@ -12,6 +13,13 @@ export const startDojoRecipeController: RequestHandler = async (req, res) => {
     const guild = await getGuildForRequest(req);
     // At this point, we know that a member of the guild is making this request. Assuming they are allowed to start a build.
     const request = JSON.parse(String(req.body)) as IStartDojoRecipeRequest;
+
+    const room = Object.values(ExportDojoRecipes.rooms).find(x => x.resultType == request.PlacedComponent.pf);
+    if (room) {
+        guild.DojoCapacity += room.capacity;
+        guild.DojoEnergy += room.energy;
+    }
+
     guild.DojoComponents!.push({
         _id: new Types.ObjectId(),
         pf: request.PlacedComponent.pf,
