07b0d37440009bd418a852a48ea097cda4f5d707
Sainan <sainan@calamity.inc>
2024-12-23 05:54:19 +0100

    fix: roomSchema not matching IRoom

--- src/models/personalRoomsModel.ts
+++ src/models/personalRoomsModel.ts
@@ -56,7 +56,7 @@ const roomSchema = new Schema<IRoom>(
     {
         Name: String,
         MaxCapacity: Number,
-        PlacedDecos: [placedDecosSchema]
+        PlacedDecos: { type: [placedDecosSchema], default: undefined }
     },
     { _id: false }
 );
