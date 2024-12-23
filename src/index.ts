063adb3519e11925b16f3ced0937249ecf9f7122
Sainan <sainan@calamity.inc>
2024-12-23 22:48:16 +0100

    improve: tell user that the WebUI is available (#631)

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,29 @@ import https from "https";
 import fs from "node:fs";
 import { app } from "./app";
 import { config } from "./services/configService";
-//const morgan = require("morgan");
-//const bodyParser = require("body-parser");
+import { connectDatabase } from "@/src/services/mongoService";
+import { registerLogFileCreationListener } from "@/src/utils/logger";
 
-const options = {
-    key: fs.readFileSync("static/certs/key.pem"),
-    cert: fs.readFileSync("static/certs/cert.pem")
-};
+registerLogFileCreationListener();
 
-const httpPort = config.httpPort || 80;
-const httpsPort = config.httpsPort || 443;
+void (async (): Promise<void> => {
+    await connectDatabase();
 
-// const server = http.createServer(app).listen(80);
-http.createServer(app).listen(httpPort, () => logger.info("HTTP server started on port " + httpPort));
-const server = https.createServer(options, app);
-server.listen(httpsPort, () => logger.info("HTTPS server started on port " + httpsPort));
+    const httpPort = config.httpPort || 80;
+    const httpsPort = config.httpsPort || 443;
+    const options = {
+        key: fs.readFileSync("static/certs/key.pem"),
+        cert: fs.readFileSync("static/certs/cert.pem")
+    };
 
-// server.keepAliveTimeout = 60 * 1000 + 1000;
-// server.headersTimeout = 60 * 1000 + 2000;
+    http.createServer(app).listen(httpPort, () => {
+        logger.info("HTTP server started on port " + httpPort);
+        https.createServer(options, app).listen(httpsPort, () => {
+            logger.info("HTTPS server started on port " + httpsPort);
+
+            logger.info(
+                "Access the WebUI in your browser at http://localhost" + (httpPort == 80 ? "" : ":" + httpPort)
+            );
+        });
+    });
+})();
