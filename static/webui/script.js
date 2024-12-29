4c5193f084f9f689de55fe6bcf26c55e14679f18
Sainan <sainan@calamity.inc>
2024-12-29 03:33:03 +0100

    improve: for "make rank 30", also make respective exalted items rank 30

--- static/webui/script.js
+++ static/webui/script.js
@@ -203,6 +203,17 @@ function updateInventory() {
                         a.onclick = function (event) {
                             event.preventDefault();
                             addGearExp("Suits", item.ItemId.$oid, 1_600_000 - item.XP);
+                            if ("exalted" in itemMap[item.ItemType]) {
+                                for (const exaltedType of itemMap[item.ItemType].exalted) {
+                                    const exaltedItem = data.SpecialItems.find(x => x.ItemType == exaltedType);
+                                    if (exaltedItem) {
+                                        const exaltedCap = itemMap[exaltedType]?.type == "weapons" ? 800_000 : 1_600_000;
+                                        if (exaltedItem.XP < exaltedCap) {
+                                            addGearExp("SpecialItems", exaltedItem.ItemId.$oid, exaltedCap - exaltedItem.XP);
+                                        }
+                                    }
+                                }
+                            }
                         };
                         a.title = "Make Rank 30";
                         a.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>`;
@@ -562,7 +573,9 @@ function addGearExp(category, oid, xp) {
             contentType: "text/plain",
             data: JSON.stringify(data)
         }).done(function () {
-            updateInventory();
+            if (category != "SpecialItems") {
+                updateInventory();
+            }
         });
     });
 }
