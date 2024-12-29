981df33601a2f74ebdd6931c1eee24c6cd6c86f8
Sainan <sainan@calamity.inc>
2024-12-29 21:42:54 +0100

    revert loadoutService

--- src/services/loadoutService.ts
+++ src/services/loadoutService.ts
@@ -1,8 +1,7 @@
 import { Loadout } from "@/src/models/inventoryModels/loadoutModel";
 import { logger } from "@/src/utils/logger";
-import { ILoadoutDatabase } from "../types/saveLoadoutTypes";
 
-export const getLoadout = async (accountId: string): Promise<ILoadoutDatabase> => {
+export const getLoadout = async (accountId: string) => {
     const loadout = await Loadout.findOne({ loadoutOwnerId: accountId });
 
     if (!loadout) {
@@ -10,5 +9,5 @@ export const getLoadout = async (accountId: string): Promise<ILoadoutDatabase> =
         throw new Error("loadout not found");
     }
 
-    return loadout.toJSON();
+    return loadout;
 };
