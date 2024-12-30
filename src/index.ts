d897f40c9211b9d6ae1324123ea8b2feab2300f9
Sainan <sainan@calamity.inc>
2024-12-30 04:11:32 +0100

    fix: abort startup if not connected to MongoDB server

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,38 @@ import https from "https";
 import fs from "node:fs";
 import { app } from "./app";
 import { config, validateConfig } from "./services/configService";
-import { connectDatabase } from "@/src/services/mongoService";
 import { registerLogFileCreationListener } from "@/src/utils/logger";
+import mongoose from "mongoose";
 
 registerLogFileCreationListener();
 validateConfig();
 
-void (async (): Promise<void> => {
-    await connectDatabase();
+mongoose
+    .connect(config.mongodbUrl)
+    .then(() => {
+        logger.info("Connected to MongoDB");
 
-    const httpPort = config.httpPort || 80;
-    const httpsPort = config.httpsPort || 443;
-    const options = {
-        key: fs.readFileSync("static/certs/key.pem"),
-        cert: fs.readFileSync("static/certs/cert.pem")
-    };
+        const httpPort = config.httpPort || 80;
+        const httpsPort = config.httpsPort || 443;
+        const options = {
+            key: fs.readFileSync("static/certs/key.pem"),
+            cert: fs.readFileSync("static/certs/cert.pem")
+        };
 
-    http.createServer(app).listen(httpPort, () => {
-        logger.info("HTTP server started on port " + httpPort);
-        https.createServer(options, app).listen(httpsPort, () => {
-            logger.info("HTTPS server started on port " + httpsPort);
+        http.createServer(app).listen(httpPort, () => {
+            logger.info("HTTP server started on port " + httpPort);
+            https.createServer(options, app).listen(httpsPort, () => {
+                logger.info("HTTPS server started on port " + httpsPort);
 
-            logger.info(
-                "Access the WebUI in your browser at http://localhost" + (httpPort == 80 ? "" : ":" + httpPort)
-            );
+                logger.info(
+                    "Access the WebUI in your browser at http://localhost" + (httpPort == 80 ? "" : ":" + httpPort)
+                );
+            });
         });
+    })
+    .catch(error => {
+        if (error instanceof Error) {
+            logger.error(`Error connecting to MongoDB server: ${error.message}`);
+        }
+        process.exit(1);
     });
-})();
