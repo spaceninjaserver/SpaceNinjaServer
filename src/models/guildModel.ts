9042d8b265debe792bdb093167121fcd6c69b25b
Sainan <sainan@calamity.inc>
2025-01-03 02:28:20 +0100

    feat: dojo research

--- src/models/guildModel.ts
+++ src/models/guildModel.ts
@@ -1,5 +1,12 @@
-import { IGuildDatabase, IDojoComponentDatabase } from "@/src/types/guildTypes";
+import {
+    IGuildDatabase,
+    IDojoComponentDatabase,
+    ITechProjectDatabase,
+    ITechProjectClient
+} from "@/src/types/guildTypes";
 import { model, Schema } from "mongoose";
+import { typeCountSchema } from "./inventoryModels/inventoryModel";
+import { toMongoDate } from "../helpers/inventoryHelpers";
 
 const dojoComponentSchema = new Schema<IDojoComponentDatabase>({
     pf: { type: String, required: true },
@@ -10,12 +17,35 @@ const dojoComponentSchema = new Schema<IDojoComponentDatabase>({
     CompletionTime: Date
 });
 
+const techProjectSchema = new Schema<ITechProjectDatabase>(
+    {
+        ItemType: String,
+        ReqCredits: Number,
+        ReqItems: [typeCountSchema],
+        State: Number,
+        CompletionDate: Date
+    },
+    { _id: false }
+);
+
+techProjectSchema.set("toJSON", {
+    virtuals: true,
+    transform(_doc, obj) {
+        const db = obj as ITechProjectDatabase;
+        const client = obj as ITechProjectClient;
+        if (db.CompletionDate) {
+            client.CompletionDate = toMongoDate(db.CompletionDate);
+        }
+    }
+});
+
 const guildSchema = new Schema<IGuildDatabase>(
     {
         Name: { type: String, required: true },
         DojoComponents: [dojoComponentSchema],
         DojoCapacity: { type: Number, default: 100 },
-        DojoEnergy: { type: Number, default: 5 }
+        DojoEnergy: { type: Number, default: 5 },
+        TechProjects: { type: [techProjectSchema], default: undefined }
     },
     { id: false }
 );
