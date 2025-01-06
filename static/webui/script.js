172db2337f6a14a29497869e14511df309f9fb83
Sainan <sainan@calamity.inc>
2025-01-06 05:36:39 +0100

    feat: infiniteEndo & infiniteRegalAya (#741)

--- static/webui/script.js
+++ static/webui/script.js
@@ -804,6 +804,8 @@ const uiConfigs = [
     "completeAllQuests",
     "infiniteCredits",
     "infinitePlatinum",
+    "infiniteEndo",
+    "infiniteRegalAya",
     "unlockAllShipFeatures",
     "unlockAllShipDecorations",
     "unlockAllFlavourItems",
