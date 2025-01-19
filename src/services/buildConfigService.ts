15f36263cde0a0a4e3068a1e68abc386ebadfc9d
Sainan <sainan@calamity.inc>
2025-01-19 12:28:45 +0100

    chore: make buildConfig.json optional (#822)

--- src/services/buildConfigService.ts
+++ src/services/buildConfigService.ts
@@ -1,13 +1,21 @@
 import path from "path";
 import fs from "fs";
 
-const rootDir = path.join(__dirname, "../..");
-const repoDir = path.basename(rootDir) == "build" ? path.join(rootDir, "..") : rootDir;
-const buildConfigPath = path.join(repoDir, "static/data/buildConfig.json");
-export const buildConfig = JSON.parse(fs.readFileSync(buildConfigPath, "utf-8")) as IBuildConfig;
-
 interface IBuildConfig {
     version: string;
     buildLabel: string;
     matchmakingBuildId: string;
+}
+
+export const buildConfig: IBuildConfig = {
+    version: "",
+    buildLabel: "",
+    matchmakingBuildId: ""
+};
+
+const rootDir = path.join(__dirname, "../..");
+const repoDir = path.basename(rootDir) == "build" ? path.join(rootDir, "..") : rootDir;
+const buildConfigPath = path.join(repoDir, "static/data/buildConfig.json");
+if (fs.existsSync(buildConfigPath)) {
+    Object.assign(buildConfig, JSON.parse(fs.readFileSync(buildConfigPath, "utf-8")) as IBuildConfig);
 }
