a8fb9095c52ea69ab9b4aa08adb1dfb713ef97c1
Sainan <sainan@calamity.inc>
2025-01-19 01:57:24 +0100

    feat: Kinematic Instant Messaging (#801)

---
+++ src/controllers/api/clearDialogueHistoryController.ts
@@ -0,0 +1,23 @@
+import { getInventory } from "@/src/services/inventoryService";
+import { getAccountIdForRequest } from "@/src/services/loginService";
+import { RequestHandler } from "express";
+
+export const clearDialogueHistoryController: RequestHandler = async (req, res) => {
+    const accountId = await getAccountIdForRequest(req);
+    const inventory = await getInventory(accountId);
+    const request = JSON.parse(String(req.body)) as IClearDialogueRequest;
+    if (inventory.DialogueHistory && inventory.DialogueHistory.Dialogues) {
+        for (const dialogueName of request.Dialogues) {
+            const index = inventory.DialogueHistory.Dialogues.findIndex(x => x.DialogueName == dialogueName);
+            if (index != -1) {
+                inventory.DialogueHistory.Dialogues.splice(index, 1);
+            }
+        }
+    }
+    await inventory.save();
+    res.end();
+};
+
+interface IClearDialogueRequest {
+    Dialogues: string[];
+}
