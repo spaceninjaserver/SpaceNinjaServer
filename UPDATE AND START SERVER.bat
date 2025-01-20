3eab0c187c6886b878a31e0f181da1a5f8137dc4
Sainan <sainan@calamity.inc>
2025-01-20 12:22:34 +0100

    feat: add update and start script for dummies (#828)

---
+++ UPDATE AND START SERVER.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo Updating SpaceNinjaServer...
+git pull
+
+if exist static\data\0\ (
+	echo Updating stripped assets...
+	cd static\data\0\
+	git pull
+	cd ..\..\..\
+)
+
+echo Updating dependencies...
+call npm i
+
+call npm run build
+call npm run start
+
+echo SpaceNinjaServer seems to have crashed.
+:a
+pause > nul
+goto a
