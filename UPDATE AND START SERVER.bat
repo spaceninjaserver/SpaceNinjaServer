f551f354fc79eb9c7adb616339f17f28952651f9
Animan8000 <aninf8000@gmail.com>
2025-01-20 13:20:07 +0100

    Updating script for dummies
    - Added some empty lines to make the console look little bit cleaner and less messy
    - Added text when it starts building the files and when the server actually starts
    - On error the window title gets changed ("Error - SpaceNinjaServer") and the color of the console turns red (though I can remove the color, if it's unhelpful/annoying), so that any idiot realizes immediately when something went wrong
    
    See screenshot for comparison

--- UPDATE AND START SERVER.bat
+++ UPDATE AND START SERVER.bat
@@ -1,22 +1,34 @@
 @echo off
 
 echo Updating SpaceNinjaServer...
+echo.
 git pull
+echo.
 
 if exist static\data\0\ (
 	echo Updating stripped assets...
 	cd static\data\0\
+	echo.
 	git pull
+	echo.
 	cd ..\..\..\
 )
 
 echo Updating dependencies...
 call npm i
+echo.
 
+echo Building files...
 call npm run build
+
+echo Starting SpaceNinjaServer...
 call npm run start
 
+echo.
+echo.
+title Error - SpaceNinjaServer
+color 0C
 echo SpaceNinjaServer seems to have crashed.
-:a
+:crashMsg
 pause > nul
-goto a
+goto crashMsg
