8633696dc850d6eba4260c12f6c7f1ed0373c4f6
Sainan <sainan@calamity.inc>
2025-02-04 19:13:48 -0800

    chore: update tunablesController (#901)
    Reviewed-on: http://209.141.38.3/OpenWF/SpaceNinjaServer/pulls/901

--- src/controllers/custom/tunablesController.ts
+++ src/controllers/custom/tunablesController.ts
@@ -1,15 +1,23 @@
 import { RequestHandler } from "express";
 
+// This endpoint is specific to the OpenWF Bootstrapper: https://openwf.io/bootstrapper-manual
+
 interface ITunables {
-    prohibit_skip_mission_start_timer?: any;
-    prohibit_fov_override?: any;
+    prohibit_skip_mission_start_timer?: boolean;
+    prohibit_fov_override?: boolean;
+    prohibit_freecam?: boolean;
+    prohibit_teleport?: boolean;
+    prohibit_scripts?: boolean;
 }
 
 const tunablesController: RequestHandler = (_req, res) => {
-    const tunablesSet: ITunables = {};
-    //tunablesSet.prohibit_skip_mission_start_timer = 1;
-    //tunablesSet.prohibit_fov_override = 1;
-    res.json(tunablesSet);
+    const tunables: ITunables = {};
+    //tunables.prohibit_skip_mission_start_timer = true;
+    //tunables.prohibit_fov_override = true;
+    //tunables.prohibit_freecam = true;
+    //tunables.prohibit_teleport = true;
+    //tunables.prohibit_scripts = true;
+    res.json(tunables);
 };
 
 export { tunablesController };
