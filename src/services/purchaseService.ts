adddc11b6fa79963b4c10ffc27467b8aea76ed0a
Sainan <sainan@calamity.inc>
2025-03-15 10:25:15 -0700

    fix: limit booster pack purchases to a max quantity of 100 (#1189)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/1189

--- src/services/purchaseService.ts
+++ src/services/purchaseService.ts
@@ -370,6 +370,11 @@ const handleBoosterPackPurchase = async (
         BoosterPackItems: "",
         InventoryChanges: {}
     };
+    if (quantity > 100) {
+        throw new Error(
+            "attempt to roll over 100 booster packs in a single go. possible but unlikely to be desirable for the user or the server."
+        );
+    }
     for (let i = 0; i != quantity; ++i) {
         for (const weights of pack.rarityWeightsPerRoll) {
             const result = getRandomWeightedRewardUc(pack.components, weights);
