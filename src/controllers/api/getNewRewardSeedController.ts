3e460c572848382deca27e5649a40891d166d145
Sainan <sainan@calamity.inc>
2025-03-18 01:45:16 -0700

    chore: update RewardSeed in database after generating a new one (#1226)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/1226

--- src/controllers/api/getNewRewardSeedController.ts
+++ src/controllers/api/getNewRewardSeedController.ts
@@ -1,7 +1,22 @@
+import { Inventory } from "@/src/models/inventoryModels/inventoryModel";
+import { getAccountIdForRequest } from "@/src/services/loginService";
+import { logger } from "@/src/utils/logger";
 import { RequestHandler } from "express";
 
-export const getNewRewardSeedController: RequestHandler = (_req, res) => {
-    res.json({ rewardSeed: generateRewardSeed() });
+export const getNewRewardSeedController: RequestHandler = async (req, res) => {
+    const accountId = await getAccountIdForRequest(req);
+
+    const rewardSeed = generateRewardSeed();
+    logger.debug(`generated new reward seed: ${rewardSeed}`);
+    await Inventory.updateOne(
+        {
+            accountOwnerId: accountId
+        },
+        {
+            RewardSeed: rewardSeed
+        }
+    );
+    res.json({ rewardSeed: rewardSeed });
 };
 
 export function generateRewardSeed(): number {
