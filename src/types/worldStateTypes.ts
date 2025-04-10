9f727789ca287392145e08d7cc62c8cf9b4a8373
Sainan <63328889+Sainan@users.noreply.github.com>
2025-04-10 12:54:48 -0700

    chore: split worldState stuff into types & service (#1548)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/1548
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

---
+++ src/types/worldStateTypes.ts
@@ -0,0 +1,122 @@
+import { IMongoDate, IOid } from "./commonTypes";
+
+export interface IWorldState {
+    Version: number; // for goals
+    BuildLabel: string;
+    Time: number;
+    Goals: IGoal[];
+    SyndicateMissions: ISyndicateMission[];
+    GlobalUpgrades: IGlobalUpgrade[];
+    Sorties: ISortie[];
+    LiteSorties: ILiteSortie[];
+    NodeOverrides: INodeOverride[];
+    EndlessXpChoices: IEndlessXpChoice[];
+    SeasonInfo: {
+        Activation: IMongoDate;
+        Expiry: IMongoDate;
+        AffiliationTag: string;
+        Season: number;
+        Phase: number;
+        Params: string;
+        ActiveChallenges: ISeasonChallenge[];
+    };
+    KnownCalendarSeasons: ICalendarSeason[];
+    Tmp?: string;
+}
+
+export interface IGoal {
+    _id: IOid;
+    Activation: IMongoDate;
+    Expiry: IMongoDate;
+    Count: number;
+    Goal: number;
+    Success: number;
+    Personal: boolean;
+    Desc: string;
+    ToolTip: string;
+    Icon: string;
+    Tag: string;
+    Node: string;
+}
+
+export interface ISyndicateMission {
+    _id: IOid;
+    Activation: IMongoDate;
+    Expiry: IMongoDate;
+    Tag: string;
+    Seed: number;
+    Nodes: string[];
+}
+
+export interface IGlobalUpgrade {
+    _id: IOid;
+    Activation: IMongoDate;
+    ExpiryDate: IMongoDate;
+    UpgradeType: string;
+    OperationType: string;
+    Value: number;
+    LocalizeTag: string;
+    LocalizeDescTag: string;
+}
+
+export interface INodeOverride {
+    _id: IOid;
+    Activation?: IMongoDate;
+    Expiry?: IMongoDate;
+    Node: string;
+    Hide?: boolean;
+    Seed?: number;
+    LevelOverride?: string;
+    Faction?: string;
+    CustomNpcEncounters?: string;
+}
+
+export interface ISortie {
+    _id: IOid;
+    Activation: IMongoDate;
+    Expiry: IMongoDate;
+    Reward: "/Lotus/Types/Game/MissionDecks/SortieRewards";
+    Seed: number;
+    Boss: string;
+    Variants: {
+        missionType: string;
+        modifierType: string;
+        node: string;
+    }[];
+}
+
+export interface ILiteSortie {
+    _id: IOid;
+    Activation: IMongoDate;
+    Expiry: IMongoDate;
+    Reward: "/Lotus/Types/Game/MissionDecks/ArchonSortieRewards";
+    Seed: number;
+    Boss: string; // "SORTIE_BOSS_AMAR" | "SORTIE_BOSS_NIRA" | "SORTIE_BOSS_BOREAL"
+    Missions: {
+        missionType: string;
+        node: string;
+    }[];
+}
+
+export interface IEndlessXpChoice {
+    Category: string;
+    Choices: string[];
+}
+
+export interface ISeasonChallenge {
+    _id: IOid;
+    Daily?: boolean;
+    Activation: IMongoDate;
+    Expiry: IMongoDate;
+    Challenge: string;
+}
+
+export interface ICalendarSeason {
+    Activation: IMongoDate;
+    Expiry: IMongoDate;
+    Season: string; // "CST_UNDEFINED" | "CST_WINTER" | "CST_SPRING" | "CST_SUMMER" | "CST_FALL"
+    Days: {
+        day: number;
+    }[];
+    YearIteration: number;
+}
