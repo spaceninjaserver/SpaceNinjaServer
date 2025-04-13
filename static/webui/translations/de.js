bef3aeed728e5ceb45532f0504ae5f2efff08a71
Animan8000 <animan8000@noreply.localhost>
2025-04-13 09:51:18 -0700

    chore(webui): update to German translation (#1615)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/1615
    Co-authored-by: Animan8000 <animan8000@noreply.localhost>
    Co-committed-by: Animan8000 <animan8000@noreply.localhost>

--- static/webui/translations/de.js
+++ static/webui/translations/de.js
@@ -137,7 +137,7 @@ dict = {
     cheats_noArgonCrystalDecay: `Argon-Kristalle verschwinden niemals`,
     cheats_noMasteryRankUpCooldown: `Keine Wartezeit beim Meisterschaftsrangaufstieg`,
     cheats_noVendorPurchaseLimits: `Keine Kaufbeschränkungen bei Händlern`,
-    cheats_noKimCooldowns: `[UNTRANSLATED] No KIM Cooldowns`,
+    cheats_noKimCooldowns: `Keine Wartezeit bei KIM`,
     cheats_instantResourceExtractorDrones: `Sofortige Ressourcen-Extraktor-Drohnen`,
     cheats_noDojoRoomBuildStage: `Kein Dojo-Raum-Bauvorgang`,
     cheats_noDojoDecoBuildStage: `Kein Dojo-Deko-Bauvorgang`,
