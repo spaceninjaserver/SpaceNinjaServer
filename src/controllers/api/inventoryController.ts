c64d466ce14d76da173516063f034a5a3b56197a
Animan8000 <animan8000@noreply.localhost>
2025-04-14 07:14:50 -0700

    fix: universalPolarityEverywhere not applying on plexus aura slot (#1614)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/1614
    Reviewed-by: Sainan <sainan@calamity.inc>
    Co-authored-by: Animan8000 <animan8000@noreply.localhost>
    Co-committed-by: Animan8000 <animan8000@noreply.localhost>

--- src/controllers/api/inventoryController.ts
+++ src/controllers/api/inventoryController.ts
@@ -202,8 +202,8 @@ export const getInventoryResponse = async (
 
     if (config.universalPolarityEverywhere) {
         const Polarity: IPolarity[] = [];
-        // 12 is needed for necramechs. 14 is needed for plexus/crewshipharness.
-        for (let i = 0; i != 14; ++i) {
+        // 12 is needed for necramechs. 15 is needed for plexus/crewshipharness.
+        for (let i = 0; i != 15; ++i) {
             Polarity.push({
                 Slot: i,
                 Value: ArtifactPolarity.Any
