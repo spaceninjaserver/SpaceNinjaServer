5f9ae2aef654311c41081d50b5637b3093a5a458
hxedcl <hxedcl@noreply.localhost>
2025-05-10 03:44:42 -0700

    chore(webui): update to Spanish translation (#2045)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/2045
    Co-authored-by: hxedcl <hxedcl@noreply.localhost>
    Co-committed-by: hxedcl <hxedcl@noreply.localhost>

--- static/webui/translations/es.js
+++ static/webui/translations/es.js
@@ -131,7 +131,7 @@ dict = {
     cheats_infiniteEndo: `Endo infinito`,
     cheats_infiniteRegalAya: `Aya Real infinita`,
     cheats_infiniteHelminthMaterials: `Materiales Helminto infinitos`,
-    cheats_claimingBlueprintRefundsIngredients: `[UNTRANSLATED] Claiming Blueprint Refunds Ingredients`,
+    cheats_claimingBlueprintRefundsIngredients: `Reclamar ingredientes devueltos por planos`,
     cheats_dontSubtractConsumables: `No restar consumibles`,
     cheats_unlockAllShipFeatures: `Desbloquear todas las funciones de nave`,
     cheats_unlockAllShipDecorations: `Desbloquear todas las decoraciones de nave`,
