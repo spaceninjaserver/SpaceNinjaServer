spaceninjaserver/SpaceNinjaServer

commit 54a73ad5d7eab867a1701ccf66d56446db96c226

fix: syndicate initiation (#2149)
Was accidentially broken by 1979b20f8cc00b79f305478f1da3d69f90a3d43e

Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/2149
Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

src/controllers/api/syndicateSacrificeController.ts
@@ -20,8 +20,8 @@ export const syndicateSacrificeController: RequestHandler = async (request, resp
 
     const oldLevel = syndicate.Title ?? 0;
     const levelIncrease = data.SacrificeLevel - oldLevel;
-    if (levelIncrease < 1) {
-        throw new Error(`syndicate sacrifice needs an increase of at least 1`);
+    if (levelIncrease < 0) {
+        throw new Error(`syndicate sacrifice can not decrease level`);
     }
     if (levelIncrease > 1 && !data.AllowMultiple) {
         throw new Error(`desired syndicate level is an increase of ${levelIncrease}, max. allowed increase is 1`);
@@ -37,7 +37,7 @@ export const syndicateSacrificeController: RequestHandler = async (request, resp
 
     // Process sacrifices and rewards for every level we're reaching
     const manifest = ExportSyndicates[data.AffiliationTag];
-    for (let level = oldLevel + 1; level <= data.SacrificeLevel; ++level) {
+    for (let level = oldLevel + levelIncrease; level <= data.SacrificeLevel; ++level) {
         let sacrifice: ISyndicateSacrifice | undefined;
         if (level == 0) {
             sacrifice = manifest.initiationSacrifice;

commit fcc11206ccd9d76c87d2d45e66b0360f4471b05f

fix: multiple syndicate level ups (#2152)
Regression from 54a73ad5d7eab867a1701ccf66d56446db96c226 because I forgot that levelIncrease could now be >1

Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/2152
Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

src/controllers/api/syndicateSacrificeController.ts
@@ -37,7 +37,7 @@ export const syndicateSacrificeController: RequestHandler = async (request, resp
 
     // Process sacrifices and rewards for every level we're reaching
     const manifest = ExportSyndicates[data.AffiliationTag];
-    for (let level = oldLevel + levelIncrease; level <= data.SacrificeLevel; ++level) {
+    for (let level = oldLevel + Math.min(levelIncrease, 1); level <= data.SacrificeLevel; ++level) {
         let sacrifice: ISyndicateSacrifice | undefined;
         if (level == 0) {
             sacrifice = manifest.initiationSacrifice;