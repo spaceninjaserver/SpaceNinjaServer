287acab8926d357017916a8962b9681de7487a39
Sainan <63328889+Sainan@users.noreply.github.com>
2025-08-27 01:30:19 -0700

    chore: abort update and start scripts when .git folder is missing (#2702)
    In that case, updating is obviously not possible.
    
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/2702
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- UPDATE AND START SERVER.bat
+++ UPDATE AND START SERVER.bat
@@ -2,24 +2,27 @@
 
 echo Updating SpaceNinjaServer...
 git fetch --prune
-git stash
-git checkout -f origin/main
-
-if exist static\data\0\ (
-	echo Updating stripped assets...
-	cd static\data\0\
-	git pull
-	cd ..\..\..\
-)
-
-echo Updating dependencies...
-call npm i --omit=dev
-
-call npm run build
 if %errorlevel% == 0 (
-	call npm run start
-	echo SpaceNinjaServer seems to have crashed.
+	git stash
+	git checkout -f origin/main
+
+	if exist static\data\0\ (
+		echo Updating stripped assets...
+		cd static\data\0\
+		git pull
+		cd ..\..\..\
+	)
+
+	echo Updating dependencies...
+	call npm i --omit=dev
+
+	call npm run build
+	if %errorlevel% == 0 (
+		call npm run start
+		echo SpaceNinjaServer seems to have crashed.
+	)
 )
+
 :a
 pause > nul
 goto a
--- UPDATE AND START SERVER.sh
+++ UPDATE AND START SERVER.sh
@@ -2,22 +2,23 @@
 
 echo "Updating SpaceNinjaServer..."
 git fetch --prune
-git stash
-git checkout -f origin/main
-
-if [ -d "static/data/0/" ]; then
-    echo "Updating stripped assets..."
-    cd static/data/0/
-    git pull
-    cd ../../../
-fi
-
-echo "Updating dependencies..."
-npm i --omit=dev
-
-npm run build
 if [ $? -eq 0 ]; then
-    npm run start
-    echo "SpaceNinjaServer seems to have crashed."
-fi
+    git stash
+    git checkout -f origin/main
 
+    if [ -d "static/data/0/" ]; then
+        echo "Updating stripped assets..."
+        cd static/data/0/
+        git pull
+        cd ../../../
+    fi
+
+    echo "Updating dependencies..."
+    npm i --omit=dev
+
+    npm run build
+    if [ $? -eq 0 ]; then
+        npm run start
+        echo "SpaceNinjaServer seems to have crashed."
+    fi
+fi
