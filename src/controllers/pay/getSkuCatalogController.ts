e6fb675e2198dce4f1f2d09fc367cb9966317011
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-17 22:37:52 -0700

    chore: update getSkuCatalog for U40 (#2905)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/2905
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/pay/getSkuCatalogController.ts
+++ src/controllers/pay/getSkuCatalogController.ts
@@ -1,5 +1,12 @@
 import type { RequestHandler } from "express";
+import { getAccountForRequest } from "../../services/loginService.ts";
+import { version_compare } from "../../helpers/inventoryHelpers.ts";
 
-export const getSkuCatalogController: RequestHandler = (_req, res) => {
-    res.sendFile("static/fixed_responses/getSkuCatalog.json", { root: "./" });
+export const getSkuCatalogController: RequestHandler = async (req, res) => {
+    const account = await getAccountForRequest(req);
+    if (!account.BuildLabel || version_compare(account.BuildLabel, "2025.10.14.16.10") >= 0) {
+        res.sendFile("static/fixed_responses/getSkuCatalogU40.json", { root: "./" });
+    } else {
+        res.sendFile("static/fixed_responses/getSkuCatalog.json", { root: "./" });
+    }
 };
