60fc6e34816d62a67ac9e473c08c6faeef55f8a8
Sainan <63328889+Sainan@users.noreply.github.com>
2025-12-18 01:00:29 -0800

    chore: update hubBlessingController (#3140)
    Fixed the cooldown and applied some changes I noticed in newer versions.
    
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3140
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/api/hubBlessingController.ts
+++ src/controllers/api/hubBlessingController.ts
@@ -1,18 +1,21 @@
+import { unixTimesInMs } from "../../constants/timeConstants.ts";
 import { getJSONfromString } from "../../helpers/stringHelpers.ts";
 import { addBooster, getInventory } from "../../services/inventoryService.ts";
 import { getAccountIdForRequest } from "../../services/loginService.ts";
 import { getRandomInt } from "../../services/rngService.ts";
 import type { RequestHandler } from "express";
 import { ExportBoosters } from "warframe-public-export-plus";
+import type { IOid } from "../../types/commonTypes.ts";
 
 export const hubBlessingController: RequestHandler = async (req, res) => {
     const accountId = await getAccountIdForRequest(req);
     const data = getJSONfromString<IHubBlessingRequest>(String(req.body));
-    const boosterType = ExportBoosters[data.booster].typeName;
     if (req.query.mode == "send") {
+        const boosterType = ExportBoosters[data.booster!].typeName;
+
         const inventory = await getInventory(accountId, "BlessingCooldown Boosters");
-        inventory.BlessingCooldown = new Date(Date.now() + 86400000);
-        addBooster(boosterType, 3 * 3600, inventory); // unfaithful, but current HUB server does not handle broadcasting, so this way users can bless themselves.
+        inventory.BlessingCooldown = new Date(Date.now() + 23 * unixTimesInMs.hour);
+        addBooster(boosterType, 3 * 3600, inventory); // unfaithful for < U41 but correct for >= U41 afaict. Either way, needed for blessings to work without a (full) HUB server.
         await inventory.save();
 
         let token = "";
@@ -23,23 +26,45 @@ export const hubBlessingController: RequestHandler = async (req, res) => {
         res.json({
             BlessingCooldown: inventory.BlessingCooldown,
             SendTime: Math.trunc(Date.now() / 1000).toString(),
+            Expiry: Math.trunc(Date.now() / 1000 + 3 * 3600).toString(), // added in 38.6.0
             Token: token
         });
     } else {
-        const inventory = await getInventory(accountId, "Boosters");
-        addBooster(boosterType, 3 * 3600, inventory);
-        await inventory.save();
+        if (data.booster) {
+            // < U41
+            const boosterType = ExportBoosters[data.booster].typeName;
 
-        res.json({
-            BoosterType: data.booster,
-            Sender: data.senderId
-        });
+            const inventory = await getInventory(accountId, "Boosters");
+            addBooster(boosterType, 3 * 3600, inventory);
+            await inventory.save();
+
+            res.json({
+                BoosterType: data.booster,
+                Sender: data.senderId
+            });
+        } else {
+            // >= U41
+            const inventory = await getInventory(accountId, "Boosters");
+            for (const blessing of data.PendingHubBlessings!) {
+                const boosterType = ExportBoosters[blessing.booster].typeName;
+                // TODO: Maybe respect sendTime?
+                addBooster(boosterType, 3 * 3600, inventory);
+            }
+            await inventory.save();
+            res.end();
+        }
     }
 };
 
 interface IHubBlessingRequest {
-    booster: string;
+    booster?: string;
     senderId?: string; // mode=request
     sendTime?: string; // mode=request
     token?: string; // mode=request
+    PendingHubBlessings?: {
+        booster: string;
+        senderId: IOid;
+        sendTime: string;
+        token: string;
+    }[];
 }
