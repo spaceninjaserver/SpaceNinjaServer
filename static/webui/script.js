1c54f79f644b328cab069097b8e8d73526ecf50d
Sainan <63328889+Sainan@users.noreply.github.com>
2026-01-01 01:20:53 -0800

    chore(webui): consistently use safe navigation when indexing itemMap (#3189)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3189
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- static/webui/script.js
+++ static/webui/script.js
@@ -1223,7 +1223,7 @@ function updateInventory() {
                     td.textContent = itemMap[item.ItemType]?.name ?? item.ItemType;
                     if (!item.Completed) {
                         td.textContent +=
-                            " | " + loc("code_stage") + ": [" + stage + "/" + itemMap[item.ItemType].chainLength + "]";
+                            " | " + loc("code_stage") + ": [" + stage + "/" + itemMap[item.ItemType]?.chainLength + "]";
                     } else {
                         td.textContent += " | " + loc("code_completed");
                     }
@@ -1262,7 +1262,7 @@ function updateInventory() {
                         a.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M463.5 224l8.5 0c13.3 0 24-10.7 24-24l0-128c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8l119.5 0z"/></svg>`;
                         td.appendChild(a);
                     }
-                    if (itemMap[item.ItemType].chainLength > stage && !item.Completed) {
+                    if (itemMap[item.ItemType]?.chainLength > stage && !item.Completed) {
                         const a = document.createElement("a");
                         a.href = "#";
                         a.onclick = function (event) {
@@ -1273,7 +1273,7 @@ function updateInventory() {
                         a.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>`;
                         td.appendChild(a);
                     }
-                    if (stage > 0 && itemMap[item.ItemType].chainLength > 1) {
+                    if (stage > 0 && itemMap[item.ItemType]?.chainLength > 1) {
                         const a = document.createElement("a");
                         a.href = "#";
                         a.onclick = function (event) {
@@ -1285,9 +1285,9 @@ function updateInventory() {
                         td.appendChild(a);
                     }
                     if (
-                        itemMap[item.ItemType].chainLength > stage &&
+                        itemMap[item.ItemType]?.chainLength > stage &&
                         !item.Completed &&
-                        itemMap[item.ItemType].chainLength > 1
+                        itemMap[item.ItemType]?.chainLength > 1
                     ) {
                         const a = document.createElement("a");
                         a.href = "#";
