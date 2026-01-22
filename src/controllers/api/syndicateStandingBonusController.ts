10676d9a21daf55672088b39501048f47c35cc7d
Sainan <63328889+Sainan@users.noreply.github.com>
2026-01-22 02:34:01 -0800

    feat: syndicateStandingBonus for older versions (#3247)
    Closes #3243
    
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3247
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/api/syndicateStandingBonusController.ts
+++ src/controllers/api/syndicateStandingBonusController.ts
@@ -8,16 +8,22 @@ import { ExportSyndicates, ExportWeapons } from "warframe-public-export-plus";
 import { logger } from "../../utils/logger.ts";
 import type { IAffiliationMods, IInventoryChanges } from "../../types/purchaseTypes.ts";
 import { EquipmentFeatures } from "../../types/equipmentTypes.ts";
+import { fromOid } from "../../helpers/inventoryHelpers.ts";
 
 export const syndicateStandingBonusController: RequestHandler = async (req, res) => {
     const accountId = await getAccountIdForRequest(req);
     const request = JSON.parse(String(req.body)) as ISyndicateStandingBonusRequest;
 
-    const syndicateMeta = ExportSyndicates[request.Operation.AffiliationTag];
+    const affiliationTag = "Operation" in request ? request.Operation.AffiliationTag : request.AffiliationTag;
+    const items = "Operation" in request ? request.Operation.Items : request.Items;
+    const modularWeaponIdRaw = "Operation" in request ? request.Operation.ModularWeaponId : request.ModularWeaponId;
+    const modularWeaponId = fromOid(modularWeaponIdRaw);
+
+    const syndicateMeta = ExportSyndicates[affiliationTag];
 
     // Process items
     let gainedStanding = 0;
-    request.Operation.Items.forEach(item => {
+    items.forEach(item => {
         const medallion = (syndicateMeta.medallions ?? []).find(medallion => medallion.itemType == item.ItemType);
         if (medallion) {
             gainedStanding += medallion.standing * item.ItemCount;
@@ -26,14 +32,14 @@ export const syndicateStandingBonusController: RequestHandler = async (req, res)
         item.ItemCount *= -1;
     });
     const inventory = await getInventory(accountId);
-    addMiscItems(inventory, request.Operation.Items);
+    addMiscItems(inventory, items);
     const inventoryChanges: IInventoryChanges = {};
-    inventoryChanges.MiscItems = request.Operation.Items;
+    inventoryChanges.MiscItems = items;
 
     // Process modular weapon
-    if (request.Operation.ModularWeaponId.$oid != "000000000000000000000000") {
+    if (modularWeaponId != "" && modularWeaponId != "000000000000000000000000") {
         const category = req.query.Category as "LongGuns" | "Pistols" | "Melee" | "OperatorAmps";
-        const weapon = inventory[category].id(request.Operation.ModularWeaponId.$oid)!;
+        const weapon = inventory[category].id(modularWeaponId)!;
         if (gainedStanding !== 0) {
             throw new Error(`modular weapon standing bonus should be mutually exclusive`);
         }
@@ -48,29 +54,39 @@ export const syndicateStandingBonusController: RequestHandler = async (req, res)
         if (weapon.Features && (weapon.Features & EquipmentFeatures.GILDED) != 0) {
             gainedStanding *= 2;
         }
-        inventoryChanges.RemovedIdItems = [{ ItemId: request.Operation.ModularWeaponId }];
-        inventory[category].pull({ _id: request.Operation.ModularWeaponId.$oid });
+        inventoryChanges.RemovedIdItems = [{ ItemId: modularWeaponIdRaw }];
+        inventory[category].pull({ _id: modularWeaponId });
         const slotBin = category == "OperatorAmps" ? InventorySlot.AMPS : InventorySlot.WEAPONS;
         freeUpSlot(inventory, slotBin);
         inventoryChanges[slotBin] = { count: -1, platinum: 0, Slots: 1 };
     }
 
     const affiliationMods: IAffiliationMods[] = [];
-    addStanding(inventory, request.Operation.AffiliationTag, gainedStanding, affiliationMods, true);
+    addStanding(inventory, affiliationTag, gainedStanding, affiliationMods, true);
 
     await inventory.save();
 
     res.json({
-        InventoryChanges: inventoryChanges,
-        AffiliationMods: affiliationMods
+        AffiliationTag: affiliationTag, // Older versions
+        StandingChange: gainedStanding, // Older versions
+        InventoryChanges: inventoryChanges, // Newer versions
+        AffiliationMods: affiliationMods // Newer versions
     });
 };
 
-interface ISyndicateStandingBonusRequest {
-    Operation: {
-        AffiliationTag: string;
-        AlternateBonusReward: ""; // ???
-        Items: IMiscItem[];
-        ModularWeaponId: IOid;
-    };
-}
+type ISyndicateStandingBonusRequest =
+    | {
+          // Newer versions
+          Operation: {
+              AffiliationTag: string;
+              AlternateBonusReward: ""; // ???
+              Items: IMiscItem[];
+              ModularWeaponId: IOid;
+          };
+      }
+    | {
+          // Older versions
+          AffiliationTag: string;
+          Items: IMiscItem[];
+          ModularWeaponId: IOid;
+      };
