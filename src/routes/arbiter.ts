43aedb94b6caf2b84ba4a5759611bab76b880567
Sainan <63328889+Sainan@users.noreply.github.com>
2026-01-27 17:48:29 +0100

    chore: move arbiter controllers to their own folder

--- src/routes/arbiter.ts
+++ src/routes/arbiter.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { dojoController, setDojoURLController } from "../controllers/api/dojoController.ts";
-import { hubController } from "../controllers/api/hubController.ts";
-import { hubInstancesController } from "../controllers/api/hubInstancesController.ts";
+import { dojoController, setDojoURLController } from "../controllers/arbiter/dojoController.ts";
+import { hubController } from "../controllers/arbiter/hubController.ts";
+import { hubInstancesController } from "../controllers/arbiter/hubInstancesController.ts";
 
 const arbiterRouter = express.Router();
 
