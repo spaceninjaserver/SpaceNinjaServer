35b04f2c37d644b2ab09617a9ba27857e77d217c
Sainan <63328889+Sainan@users.noreply.github.com>
2026-03-07 00:54:35 -0800

    feat: convert old clan hall to new one (#3475)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3475
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

---
+++ src/controllers/api/changeDojoSpawnRoomController.ts
@@ -0,0 +1,25 @@
+import type { RequestHandler } from "express";
+//import type { IOidWithLegacySupport } from "../../types/commonTypes.ts";
+import { getAccountForRequest } from "../../services/loginService.ts";
+import { getInventory } from "../../services/inventoryService.ts";
+import { getDojoClient, getGuildForRequestEx } from "../../services/guildService.ts";
+
+export const changeDojoSpawnRoomController: RequestHandler = async (req, res) => {
+    const account = await getAccountForRequest(req);
+    const inventory = await getInventory(account._id.toString(), "GuildId");
+    const guild = await getGuildForRequestEx(req, inventory);
+    if (guild.DojoComponents.length != 1) {
+        res.status(400).end();
+    }
+    guild.DojoComponents[0].pf = "/Lotus/Levels/ClanDojo/DojoHall.level";
+    await guild.save();
+    res.json(await getDojoClient(guild, 0, undefined, account.BuildLabel));
+};
+
+/*interface IChangeDojoSpawnRoomRequest {
+    SpawnComponent: {
+        id: IOidWithLegacySupport;
+        pf: "/Lotus/Levels/ClanDojo/DojoHall.level";
+        ppf?: "";
+    };
+}*/
