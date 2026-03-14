14c1ff0032477c8b6fb91ba537251065aa8684e5
Sainan <63328889+Sainan@users.noreply.github.com>
2026-03-14 01:43:42 -0700

    chore: handle getAccountId with unicode suffix (#3543)
    Closes #3539
    
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3543
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/api/getAccountIdController.ts
+++ src/controllers/api/getAccountIdController.ts
@@ -3,7 +3,10 @@ import { getJSONfromString } from "../../helpers/stringHelpers.ts";
 import { Account } from "../../models/loginModel.ts";
 
 export const getAccountIdController: RequestHandler = async (req, res) => {
-    const { targetName } = getJSONfromString<IGetAccountIdRequest>(String(req.body));
+    let { targetName } = getJSONfromString<IGetAccountIdRequest>(String(req.body));
+    if (targetName.charCodeAt(targetName.length - 1) >= 0xe000) {
+        targetName = targetName.substring(0, targetName.length - 1);
+    }
     const account = await Account.findOne({ DisplayName: targetName }, "_id DisplayName");
     if (account) {
         res.send(`${account._id.toString()},${account.DisplayName}`);
