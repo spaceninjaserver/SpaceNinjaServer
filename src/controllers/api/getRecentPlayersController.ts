2c3429e6057d6f9957d77c6636a318987dfb4d36
Sainan <63328889+Sainan@users.noreply.github.com>
2026-03-14 01:44:21 -0700

    feat: handle empty id provided in getRecentPlayers (#3552)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3552
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/api/getRecentPlayersController.ts
+++ src/controllers/api/getRecentPlayersController.ts
@@ -3,12 +3,22 @@ import type { IFriendInfo } from "../../types/friendTypes.ts";
 import { getJSONfromString } from "../../helpers/stringHelpers.ts";
 import { addAccountDataToFriendInfo, addInventoryDataToFriendInfo } from "../../services/friendService.ts";
 import { getAccountForRequest } from "../../services/loginService.ts";
+import { fromOid, toOid2 } from "../../helpers/inventoryHelpers.ts";
+import { Account } from "../../models/loginModel.ts";
 
 export const getRecentPlayersController: RequestHandler = async (req, res): Promise<void> => {
     const account = await getAccountForRequest(req);
     const payload = getJSONfromString<IRecentPlayersPayload>(String(req.body));
     const promises: Promise<void>[] = [];
     for (const info of payload.RecentPlayers) {
+        if (!fromOid(info._id)) {
+            // U18 may provide an empty $id but with a DisplayName for us to look us.
+            const otherAccount = await Account.findOne({ DisplayName: info.DisplayName }, "_id");
+            if (!otherAccount) {
+                continue;
+            }
+            info._id = toOid2(otherAccount._id, account.BuildLabel);
+        }
         promises.push(addAccountDataToFriendInfo(info, account.BuildLabel));
         promises.push(addInventoryDataToFriendInfo(info));
     }
