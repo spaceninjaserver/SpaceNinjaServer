02714f5325a71a299b1e0cdfd728bb2a7e56c907
Sainan <63328889+Sainan@users.noreply.github.com>
2026-03-27 02:05:17 -0700

    feat: joining foreign sessions (#3656)
    Given the current situation of people sharing the same NRS without necessarily sharing the same SNS, this seems like a neat albeit niche capability to have.
    
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3656
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/api/findSessionsController.ts
+++ src/controllers/api/findSessionsController.ts
@@ -2,12 +2,34 @@ import type { RequestHandler } from "express";
 import { getSession } from "../../managers/sessionManager.ts";
 import { logger } from "../../utils/logger.ts";
 import type { IFindSessionRequest } from "../../types/session.ts";
+import { getNrsAddress } from "../../services/configService.ts";
 
-export const findSessionsController: RequestHandler = (_req, res) => {
+export const findSessionsController: RequestHandler = async (_req, res) => {
     const req = JSON.parse(String(_req.body)) as IFindSessionRequest;
     logger.debug("FindSession Request:", req);
     const sessions = getSession(req);
+
+    if (!sessions.length && "id" in req) {
+        // (OpenWF-specific) Maybe NRS can tell us who the host of this session is...
+        logger.debug(`Unknown session id, asking NRS...`);
+        try {
+            const [nrsAddr, nrsPort] = getNrsAddress();
+            const res = await fetch(`http://${nrsAddr}:${nrsPort}/api/session/${req.id}`);
+            const json = (await res.json()) as INrsSessionInfo;
+            if (json.hid) {
+                sessions.push({ id: req.id, createdBy: json.hid });
+            }
+        } catch (e) {
+            /* empty */
+        }
+    }
+
     logger.debug("FindSession Result:", { sessions });
     if (sessions.length) res.json({ queryId: req.queryId, Sessions: sessions });
     else res.json({});
 };
+
+interface INrsSessionInfo {
+    id?: string;
+    hid?: string;
+}
--- src/controllers/api/joinSessionController.ts
+++ src/controllers/api/joinSessionController.ts
@@ -3,21 +3,34 @@ import { getSessionByID } from "../../managers/sessionManager.ts";
 import { logger } from "../../utils/logger.ts";
 import { getAccountForRequest } from "../../services/loginService.ts";
 import { toOid2 } from "../../helpers/inventoryHelpers.ts";
+import { generateRewardSeed } from "../../services/rngService.ts";
 
 export const joinSessionGetController: RequestHandler = async (req, res) => {
     const account = await getAccountForRequest(req);
-    const session = getSessionByID(req.query.sessionId as string);
-    if (session) res.json({ rewardSeed: session.rewardSeed, sessionId: toOid2(session.sessionId, account.BuildLabel) });
-    else res.json({});
+    const sessionId = req.query.sessionId as string;
+    const session = getSessionByID(sessionId);
+    if (!session) {
+        logger.warn(`joining an unknown session; rewardSeed will not be in sync`);
+    }
+    res.json({
+        rewardSeed: session?.rewardSeed ?? generateRewardSeed(),
+        sessionId: toOid2(sessionId, account.BuildLabel)
+    });
 };
 
 export const joinSessionPostController: RequestHandler = async (req, res) => {
     const account = await getAccountForRequest(req);
     const reqBody = JSON.parse(String(req.body)) as IJoinSessionRequest;
     logger.debug(`JoinSession Request`, { reqBody });
-    const session = getSessionByID(reqBody.sessionIds[0]);
-    if (session) res.json({ rewardSeed: session.rewardSeed, sessionId: toOid2(session.sessionId, account.BuildLabel) });
-    else res.json({});
+    const sessionId = reqBody.sessionIds[0];
+    const session = getSessionByID(sessionId);
+    if (!session) {
+        logger.warn(`joining an unknown session; rewardSeed will not be in sync`);
+    }
+    res.json({
+        rewardSeed: session?.rewardSeed ?? generateRewardSeed(),
+        sessionId: toOid2(sessionId, account.BuildLabel)
+    });
 };
 
 interface IJoinSessionRequest {
