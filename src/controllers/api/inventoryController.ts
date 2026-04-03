c8a570ccdabe4d021a8a6e0abc35808929c3f936
Sainan <63328889+Sainan@users.noreply.github.com>
2026-04-03 05:48:14 -0700

    fix: give 2 extra trades to founders at daily reset (#3685)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/3685
    Co-authored-by: Sainan <63328889+Sainan@users.noreply.github.com>
    Co-committed-by: Sainan <63328889+Sainan@users.noreply.github.com>

--- src/controllers/api/inventoryController.ts
+++ src/controllers/api/inventoryController.ts
@@ -77,6 +77,9 @@ export const inventoryController: RequestHandler = async (request, response) => 
         inventory.DailyFocus = 250000 + inventory.PlayerLevel * 5000;
         inventory.GiftsRemaining = Math.max(8, inventory.PlayerLevel);
         inventory.TradesRemaining = inventory.PlayerLevel;
+        if (inventory.Founder) {
+            inventory.TradesRemaining += 2;
+        }
 
         inventory.LibraryAvailableDailyTaskInfo = createLibraryDailyTask();
 
