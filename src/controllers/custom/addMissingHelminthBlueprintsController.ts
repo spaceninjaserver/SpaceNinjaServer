7ab33461b292ee056e7407951ccc175222f20e7c
BanLanGen <200+banlangen@noreply.localhost>
2026-04-29 14:31:41 -0700

    chore: add flowers to helminth with "add missing subsumed abilities" (#4004)
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/4004
    Reviewed-by: Sainan <63328889+sainan@users.noreply.github.com>
    Co-authored-by: BanLanGen <200+banlangen@noreply.localhost>
    Co-committed-by: BanLanGen <200+banlangen@noreply.localhost>

--- src/controllers/custom/addMissingHelminthBlueprintsController.ts
+++ src/controllers/custom/addMissingHelminthBlueprintsController.ts
@@ -6,19 +6,26 @@ import { broadcastInventoryUpdate } from "../../services/wsService.ts";
 
 export const addMissingHelminthBlueprintsController: RequestHandler = async (req, res) => {
     const accountId = await getAccountIdForRequest(req);
-    const inventory = await getInventory(accountId, "Recipes");
-    const allHelminthRecipes = Object.keys(ExportRecipes).filter(
-        key => ExportRecipes[key].secretIngredientAction === "SIA_WARFRAME_ABILITY"
-    );
-    const inventoryHelminthRecipes = inventory.Recipes.filter(recipe =>
-        recipe.ItemType.startsWith("/Lotus/Types/Recipes/AbilityOverrides/")
-    ).map(recipe => recipe.ItemType);
+    const inventory = await getInventory(accountId, "Recipes InfestedFoundry");
 
-    const missingHelminthRecipes = allHelminthRecipes
-        .filter(key => !inventoryHelminthRecipes.includes(key))
-        .map(ItemType => ({ ItemType, ItemCount: 1 }));
+    for (const [recipeType, recipe] of Object.entries(ExportRecipes)) {
+        if (recipe.secretIngredientAction == "SIA_WARFRAME_ABILITY") {
+            if (!inventory.Recipes.some(recipe => recipe.ItemType == recipeType)) {
+                //logger.debug(`adding recipe: ${recipeType}`);
+                addRecipes(inventory, [{ ItemType: recipeType, ItemCount: 1 }]);
 
-    addRecipes(inventory, missingHelminthRecipes);
+                const suitType = recipe.secretIngredients!.find(x =>
+                    x.ItemType.startsWith("/Lotus/Powersuits")
+                )?.ItemType;
+                if (suitType) {
+                    //logger.debug(`adding consumed suit: ${suitType}`);
+                    inventory.InfestedFoundry ??= {};
+                    inventory.InfestedFoundry.ConsumedSuits ??= [];
+                    inventory.InfestedFoundry.ConsumedSuits.push({ s: suitType });
+                }
+            }
+        }
+    }
 
     await inventory.save();
     res.end();
