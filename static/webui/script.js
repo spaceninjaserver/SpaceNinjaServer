ad28b7e3c6758ccab0f476fd6f7fa9061e15bc73
Syzyf21 <392+syzyf21@noreply.localhost>
2026-08-23 09:35:08 -0700

    feat(webui): pl translation (#4494)
    Co-authored-by: Syzyf21 <hubert.nowacki21@pm.me>
    Co-authored-by: syzyf21 <hubert.nowacki21@pm.me>
    Reviewed-on: https://onlyg.it/OpenWF/SpaceNinjaServer/pulls/4494
    Co-authored-by: Syzyf21 <392+syzyf21@noreply.localhost>
    Co-committed-by: Syzyf21 <392+syzyf21@noreply.localhost>

--- static/webui/script.js
+++ static/webui/script.js
@@ -510,7 +510,7 @@ function setActiveLanguage(lang) {
     document.querySelector("[data-lang=" + lang + "]").classList.add("active");
 
     window.dictPromise = new Promise(resolve => {
-        const webui_lang = ["en", "ru", "fr", "de", "zh", "es", "uk"].indexOf(lang) == -1 ? "en" : lang;
+        const webui_lang = ["en", "ru", "fr", "de", "zh", "es", "uk", "pl"].indexOf(lang) == -1 ? "en" : lang;
         let script = document.getElementById("translations");
         if (script) document.documentElement.removeChild(script);
 
